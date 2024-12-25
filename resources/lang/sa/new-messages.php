a0a94ba5975ad0f8dbc0232facf1cc25f1aa8580
AbdouShalby <abdoushalby@gmail.com>
2024-12-25 17:06:38 +0200

    Initial commit

---
+++ resources/lang/sa/new-messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	"00000000000000" => "00000000000000",
+	"image_alt_text" => "Image alt text",
+	"apex_Brand" => "Apex Brand",
+	"search_by_brand_name" => "Search by brand name",
+	"_products_under_this_brand" => " products under this brand",
+];
